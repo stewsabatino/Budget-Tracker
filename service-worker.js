c21ac8cc174ada322cc2b00a0713638a19b76081
Stew Sabatino <stewsabatino@gmail.com>
2021-11-23 19:32:49 -0600

    initial commit

---
+++ service-worker.js
@@ -0,0 +1,65 @@
+const CACHE_NAME = "static-cache-v2"
+const DATA_CACHE_NAME = "data-cache-v1"
+const FILES_TO_CACHE = [
+    "/",
+    "/index.js",
+    "/db.js",
+    "style.css",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png",
+    "manifest.webmanifest",
+    "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+]
+
+// install
+self.addEventListener("install", function (event) {
+    event.waitUntil(caches
+        .open(CACHE_NAME)
+        .then((cache) => cache.addAll(FILES_TO_CACHE)))
+    self.skipWaiting()
+})
+
+// activating
+self.addEventListener("activate", function (event) {
+    event.waitUntil(
+        caches.keys().then(keyList => {
+            return Promise.all(
+                keyList.map(key => {
+                    if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+                        console.log("Removing old cache data", key);
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
+    );
+    self.clients.claim();
+});
+
+// fetching
+self.addEventListener("fetch", function (event) {
+    if (event.request.url.includes("/api/")) {
+        event.respondWith(
+            caches.open(DATA_CACHE_NAME).then(cache => {
+                return fetch(event.request)
+                    .then(response => {
+                        if (response.status === 200) {
+                            cache.put(event.request.url, response.clone());
+                        }
+                        return response;
+                    })
+                    .catch(err => {
+                        return cache.match(event.request);
+                    });
+            }).catch(err => console.log(err))
+        );
+        return;
+    }
+    event.respondWith(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.match(event.request).then(response => {
+                return response || fetch(event.request);
+            });
+        })
+    );
+});
