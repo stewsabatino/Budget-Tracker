74893c73229edc3e2f5656074b6111ca3d0bd382
Stew Sabatino <stewsabatino@gmail.com>
2021-11-23 20:03:32 -0600

    script for service worker / link for manifest to html

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,7 +4,7 @@ const FILES_TO_CACHE = [
     "/",
     "/index.js",
     "/db.js",
-    "style.css",
+    "styles.css",
     "/icons/icon-192x192.png",
     "/icons/icon-512x512.png",
     "manifest.webmanifest",
@@ -13,9 +13,8 @@ const FILES_TO_CACHE = [
 
 // install
 self.addEventListener("install", function (event) {
-    event.waitUntil(caches
-        .open(CACHE_NAME)
-        .then((cache) => cache.addAll(FILES_TO_CACHE)))
+    event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE)))
+    
     self.skipWaiting()
 })
 
